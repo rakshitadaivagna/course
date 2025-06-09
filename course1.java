459ac6065d120ed5bc9fa14d9ab36d9fca18a192
rakshitadaivagna <rakshitadaivagna.gmail.com>
2025-06-09 13:02:36 +0530

    save file

---
+++ course1.java
@@ -0,0 +1,35 @@
+public class course {
+    course(string name,int marks){
+        this.string name=name;
+        this.int marks=marks;
+    }
+}
+
+public class student {
+    string name = "priya";
+    string program = "devops";
+    int semester = 4;}
+course[]course={
+    new course(name="java",marks=95);
+};
+
+void display
+
+    course marks(){
+    system.out.println(x:"\n course with marks");
+    for(course c:courses){
+        if(ci marks<40){
+            system.out.println("  "+(course))
+        }
+    }
+}
+
+    public class main {
+    public static void main(string[]args){
+        student s=newstudent;
+
+        s.display info();
+
+        s.display highermarks();
+    }
+}
